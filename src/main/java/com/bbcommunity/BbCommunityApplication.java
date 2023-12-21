10990fd5e975aeb9588fe16aa974a6d80a9d8a8f
김진광 <113023365+kjink98@users.noreply.github.com>
2023-12-21 15:31:05 +0900

    STS 생성

---
+++ src/main/java/com/bbcommunity/BbCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.bbcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbcommunity/BbCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
