0f295059a7e4a354a44d706d731e0010968d9b89
kjink98 <kjink98@naver.com>
2023-12-30 15:59:49 +0900

    내 정보 확인

--- src/main/java/com/bbcommunity/controller/UserViewController.java
+++ src/main/java/com/bbcommunity/controller/UserViewController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bbcommunity.dto.UserRegisterDto;
+import com.bbcommunity.entity.User;
+import com.bbcommunity.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserViewController {
+	private final UserService userService;
 	
 	@GetMapping("/register")
 	public String register(Model model) {
@@ -21,4 +27,23 @@ public class UserViewController {
 	public String login() {
 	    return "user/login";
 	}
+	
+	@GetMapping("/myInfo")
+	public String myInfo(Model model) {
+        // 현재 로그인 중인 사용자의 Member 엔티티를 가져옴
+        User loggedInUser = userService.getCurrentLoggedInMember();
+        if (loggedInUser != null) {
+            model.addAttribute("name", loggedInUser.getName());
+            model.addAttribute("email", loggedInUser.getEmail());
+            model.addAttribute("gender", loggedInUser.getGender());
+            model.addAttribute("nickname", loggedInUser.getNickname());
+            model.addAttribute("regdate", loggedInUser.getRegdate());
+            model.addAttribute("role", loggedInUser.getRole());
+        } else {
+            // 로그인 안됐을때 로그인 페이지로 이동시키기
+        	return "user/login";
+        }
+
+        return "user/myInfo";
+    }
 }
