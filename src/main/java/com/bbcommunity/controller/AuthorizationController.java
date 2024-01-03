ed4c560074af90a2c4bea98a1070e327a4536cd1
kjink98 <kjink98@naver.com>
2024-01-03 15:39:25 +0900

    회원가입 성공 페이지

--- src/main/java/com/bbcommunity/controller/AuthorizationController.java
+++ src/main/java/com/bbcommunity/controller/AuthorizationController.java
@@ -1,17 +1,16 @@
 package com.bbcommunity.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.bbcommunity.dto.UserRegisterDto;
 import com.bbcommunity.entity.User;
 import com.bbcommunity.service.RegisterUserService;
 
-@RestController
+@Controller
 @RequestMapping("/user")
 public class AuthorizationController {
 	private final RegisterUserService registerUserService;
@@ -21,19 +20,23 @@ public class AuthorizationController {
 	}
 
 	@PostMapping("/register")
-    public ResponseEntity<?> register(@ModelAttribute UserRegisterDto userRegisterDto) {
-        try {
-				User user = registerUserService.register(
-		            userRegisterDto.getEmail(),
-		            userRegisterDto.getPassword(),
-		            userRegisterDto.getPasswordCheck(),
-		            userRegisterDto.getName(),
-		            userRegisterDto.getGender(),
-		            userRegisterDto.getNickname()
-			        );
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
+	public String register(@ModelAttribute UserRegisterDto userRegisterDto, Model model) {
+	    try {
+	        User user = registerUserService.register(
+	            userRegisterDto.getEmail(),
+	            userRegisterDto.getPassword(),
+	            userRegisterDto.getPasswordCheck(),
+	            userRegisterDto.getName(),
+	            userRegisterDto.getGender(),
+	            userRegisterDto.getNickname()
+	        );
+
+	        model.addAttribute("message", "회원가입이 완료되었습니다.");
+	        return "user/registerSuccess";  // Thymeleaf 템플릿 이름
+	    } catch (Exception e) {
+	        model.addAttribute("message", e.getMessage());
+	        return "registerError";  // Thymeleaf 템플릿 이름
+	    }
+	}
+
 }
