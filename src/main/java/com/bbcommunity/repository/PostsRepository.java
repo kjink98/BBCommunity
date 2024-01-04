2cb0eb9df39aed9febce6526fa078671a783743d
kjink98 <kjink98@naver.com>
2024-01-04 22:04:23 +0900

    Apply .gitignore

--- src/main/java/com/bbcommunity/repository/PostsRepository.java
+++ src/main/java/com/bbcommunity/repository/PostsRepository.java
@@ -16,14 +16,7 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
 	Page<Posts> findByBoardBoardId(Long boardId, Pageable pageable);
 	
 	Optional<Posts> findByPostId(Long id);
-	/**
-     * 모든 글을 내림차순으로 조회하는 쿼리입니다.
-     */
-//	List<Posts> findAllByOrderByIdDesc();
 	
-	/**
-     * 제목에 특정 단어를 포함하는 글을 대소문자 구분 없이 조회하는 쿼리입니다.
-     */
 	Page<Posts> findByTitleIgnoreCaseContaining(String title, Pageable pageable);
 
 	
@@ -31,13 +24,4 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
 	
 	
 	
-	/**
-     * 조회수 올려주는 쿼리
-     
-	@Modifying
-	@Query("")
-	int updateView(@Param("id") Long id);
-	
-	List<Posts> findByAuthorOrderByIdDesc(String userId);
-	*/
 }
